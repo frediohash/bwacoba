455daf6960c4c9122b3e9374b38df3ac59fc25df
fredio.dimyati <fredio.dimyati@dwidasa.com>
2023-02-14 06:40:32 +0700

    update

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bwacoba/users"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/bwacoba?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println("Connected Successfully")
+
+	var users []users.Users
+	length := len(users)
+	fmt.Println(length)
+
+	db.Find(&users)
+	length = len(users)
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("=======================")
+	}
+}
